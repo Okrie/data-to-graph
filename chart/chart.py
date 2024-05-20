cabe9f00dbe389454aafb46538ac7ddd12e694b6
Sumin, Kim <mykkang55@naver.com>
2024-05-20 16:44:57 +0900

    Line 그래프 분리 작업 구현 완료
    - Line 기본 틀 관련 분리 작업 구현 완료
    - 기본적으로 옵션이 없다면 기본 옵션을 따라간다.

--- chart/chart.py
+++ chart/chart.py
@@ -9,54 +9,67 @@ class drawChart:
     _len: int = 0
     
     # OPTIONS
-    general: dict = {
-        # Common GRAPH ARGS
-        'graph_style': 'ggplot',    # graph view style
-        
-        ## GRAPH SIZE
-        'fig_size': (12, 5),    # graph size        tuple
-        
-        ## GRAPH View Settings
-        ### GRAPH TEXT
-        'title': None,          # graph title                   str
-        'label': _data.columns if _data != None else None, # legend label                  list | series
-        
-        # Flip x - y
-        'flip': False,          # Flip X-Y axis                 True, False
+    DEFAULT_OPTION: dict = {
+        'general': {
+            # Common GRAPH ARGS
+            'graph_style': 'ggplot',    # graph view style
+            
+            ## GRAPH SIZE
+            'fig_size': (12, 5),    # graph size        tuple
+            
+            ## GRAPH View Settings
+            ### GRAPH TEXT
+            'title': None,          # graph title                   str
+            'label': _data.columns if _data != None else None, # legend label                  list | series
+            
+            # Flip x - y
+            'flip': False,          # Flip X-Y axis                 True, False
+            
+            # DropColumns
+            'drop_columns': False,  # Drop Column on Graph          True, False
+            'drop_columns_name': [''], # Drop Column Name           list[str]
+            'drop_column_axis': 0,  # Drop Column Axis              0 - horizental, 1 - vertical
+            
+            # Graph resolution
+            'dpi': 200,             # Graph Resolution              default = 200
+        },
+        'x_axis': {
+            'label': None,          # x axis label                  str
+            'ticks': 45,            # x label text rotate degree    -90 ~ 90
+            'min' : None,              # limit low value
+            'max' : None,           # limit high value
+        },
+        'y_axis': {
+            'label': None,          # y axis label                  str
+            'ticks': 0,             # y label text rotate degree    -90 ~ 90
+            'min' : None,              # limit low value
+            'max' : None,           # limit high value
+        },
+        'legend': {
+            'labels': None,         # legend label                  columns
+            'location': 'best',     # legend location               best, left, center, right, upper [left, center, right], lower [left, center, right]
+            'fontsize': 7,          # legend fontsize               int
+        },
+        'overlay': {
+            ### GRID, AXIS
+            'grid': True,           # graph in grid background      True, False
+            'axis': 0,              # value axis                    0 - horizental, 1 - vertical
+            
+            ## GRAPH layout margin
+            'tight_layout': False,  # graph layout margin           True, False
+        },
+        'line': {
+            'width': 1,             # Line Width                    float over 0
+            'style': '-',           # Line Style                    default = '-', '--' '-.' ':'
+            'colors': None,         # Line Colors                   default following matplotlib colors    
+            # Marker
+            'marker': 'o',          # draw line on marker           
+            'marker_size': 5,       # marker size
+        },
     }
     
-    x_axis: dict = {
-        'label': None,          # x axis label                  str
-        'ticks': 45,            # x label text rotate degree    -90 ~ 90
-        'min' : 0,              # limit low value
-        'max' : 1000,           # limit high value
-    }
     
-    y_axis: dict = {
-        'label': None,          # y axis label                  str
-        'ticks': 0,             # y label text rotate degree    -90 ~ 90
-        'min' : 0,              # limit low value
-        'max' : 1000,           # limit high value
-    }
     
-    legend: dict = {
-        'location': 'best',     # legend location               best, left, center, right, upper [left, center, right], lower [left, center, right]
-        'fontsize': 7,          # legend fontsize               int
-    }
-    
-    overlay: dict = {
-        ### COLOR, WIDTH, HEIGHT, GRID, AXIS
-        'color': None,          # graph value color             str | list
-        'grid': True,           # graph in grid background      True, False
-        'axis': 0,              # value axis                    0 - horizental, 1 - vertical
-        
-        ## GRAPH layout margin
-        'tight_layout': False,  # graph layout margin           True, False
-        
-        # Marker
-        'marker': 'o',          # draw line on marker           
-        'marker_size': 5,       # marker size
-    }
     
     def __init__(self):
         # print(self._data)
@@ -119,23 +132,146 @@ class drawChart:
         return datetime.fromisoformat(beforeDatetime[:-5]).strftime('%Y-%m-%d %H:%M:%S')
     
     # option 정의 재사용을 위한 함수
-    def optionUpdate(self, **kwargs):
-        self.general.update(kwargs.pop('general', self.general))
-        self.x_axis.update(kwargs.pop('x_axis', self.x_axis))
-        self.y_axis.update(kwargs.pop('y_axis', self.y_axis))
-        self.legend.update(kwargs.pop('legend', self.legend))
-        self.overlay.update(kwargs.pop('overlay', self.overlay))
+    def optionUpdate(self, default_option, overrides_option):
+        for k, v in overrides_option.items():
+            if isinstance(v, dict) and k in default_option:
+                default_option[k] = self.optionUpdate(default_option.get(k, {}), v)
+            else:
+                default_option[k] = v
+        return default_option
     
     # 05.17 그래프 분리
     # Line Graph
-    def line(self, general: dict, x_axis: dict, y_axis: dict, legend: dict, overlay: dict):
-        self.optionUpdate(general, x_axis, y_axis, legend, overlay)
+    def line(self, option = dict):
+        """## Line Graph
+
+        ### Args:
+            general (dict): 
+                ```
+                'general': {
+                    'graph_style': 'ggplot', # graph view style
+                    'fig_size': (12, 5), # graph size  tuple
+                    'title': None, # graph title  str
+                    'label': _data.columns if _data != None else None, # legend label  list | series
+                    'flip': False, # Flip X-Y axis  True, False
+                    'drop_columns': False, # Drop Column on Graph  True, False
+                    'drop_columns_name': [''], # Drop Column Name  list[str]
+                    'drop_column_axis': 0, # Drop Column Axis  0 - horizental, 1 - vertical
+                    'dpi': 200, # Graph Resolution  default = 200
+                }
+                ```
+            x_axis (dict): 
+                ```
+                'x_axis': {
+                    'label': None, # x axis label  str
+                    'ticks': 45, # x label text rotate degree  -90 ~ 90
+                    'min' : None, # limit low value
+                    'max' : None, # limit high value
+                }
+                ```
+            y_axis (dict):
+                ```
+                'y_axis': {
+                    'label': None, # y axis label  str
+                    'ticks': 0, # y label text rotate degree  -90 ~ 90
+                    'min' : None, # limit low value
+                    'max' : None, # limit high value
+                }
+                ```
+            legend (dict): 
+                ```
+                'legend': {
+                    'labels': None, # legend label  columns
+                    'location': 'best', # legend location  best, left, center, right, upper [left, center, right], lower [left, center, right]
+                    'fontsize': 7, # legend fontsize  int
+                }
+                ```
+            overlay (dict): 
+                ```
+                'overlay': {
+                    'grid': True, # graph in grid background  True, False
+                    'axis': 0, # value axis  0 - horizental, 1 - vertical
+                    'tight_layout': False, # graph layout margin  True, False
+                }
+                ```
+            line (dict):
+                ```
+                'line': {
+                    'width': 1, # Line Width  float over 0
+                    'style': '-', # Line Style  default = '-', '--' '-.' ':'
+                    'colors': None, # Line Colors  default following matplotlib colors    
+                    'marker': 'o', # draw line on marker           
+                    'marker_size': 5, # marker size
+                }
+                ```
+        """
+        updated_option = self.optionUpdate(self.DEFAULT_OPTION.copy(), option)
         
+        print(f'Option \n {updated_option}')
+        general = updated_option['general']
+        x_axis = updated_option['x_axis']
+        y_axis = updated_option['y_axis']
+        legend = updated_option['legend']
+        overlay = updated_option['overlay']
+        line = updated_option['line']
         
-        return
+        plt.style.use(general['graph_style'])
+        
+        # 원본 보존을 위한 copy
+        data = self._data.copy()
+        
+        # column 삭제 유무
+        if general['drop_columns']:
+            data.T.drop(general['drop_columns_name'], axis=general['drop_column_axis'], inplace=True)
+            print(data)
+        
+        # base64 Encoding을 위한 bytesIO
+        __graphToBytes = BytesIO()
+
+        # Draw Graph
+        dfGraph = data.T.copy() if not general['flip'] else data.copy()
+        fig = plt.figure(figsize=general['fig_size'])
+        
+        ax = fig.add_subplot(1, 1, 1)
+        
+        print(dfGraph.T)
+        
+        for i in range(len(data.columns)):
+            if line['colors']:
+                color = line['colors'][i % len(line['colors'])]
+            else:
+                color = None
+            ax.plot(
+                dfGraph.columns,
+                dfGraph.loc[data.columns[i], :],
+                marker= line['marker'],
+                markersize= line['marker_size'],
+                lw= line['width'],
+                color= color
+            )
+        
+        plt.title(general['title'], loc='center')
+        plt.xlim((x_axis['min'], x_axis['max']))
+        plt.ylim((y_axis['min'], y_axis['max']))
+        plt.xlabel(x_axis['label'])
+        plt.ylabel(y_axis['label'])
+        plt.xticks(rotation = x_axis['ticks'])
+        plt.yticks(rotation = y_axis['ticks'])
+        plt.grid(visible=overlay['grid'])
+        plt.legend(labels = data.columns if legend['labels'] == None else legend['labels'], loc=legend['location'], fontsize=legend['fontsize'])
+        
+        # Graph PNG Setting
+        plt.savefig(__graphToBytes, format='png', dpi=200, bbox_inches='tight')
+        plt.close()
+        
+        # Base64 encoding
+        import base64
+        __convBase64 = base64.b64encode(__graphToBytes.getvalue()).decode("utf-8").replace("\n", "")
+        return "data:image/png;base64,%s" % __convBase64
     
-    # 현재 진행 중
+    
     # 05.14 각 도표별 필요 인자 및 설정 할 수 있는 인자 값 초기화 및 정리, 설명 추가 진행 중
+    # 05.20 개별로 함수 분리 작업 진행 중
     # draw graph
     def drawGraph(self, graphStyle : plt.style.available = 'ggplot', **kwargs):
         """
